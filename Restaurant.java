fa514c3294b218a47ff6a8dbbcf47ef3019ec997
Weyrd <wyrd.reisa@gmail.com>
2022-03-24 10:40:08 +0100

    update

--- Restaurant.java
+++ Restaurant.java
@@ -1,8 +1,12 @@
-import models.employes.*;
+import models.*;
 import models.boissons.*;
+import models.employes.*;
 import models.plats.*;
+import java.util.*;
 
 public class Restaurant {
+    public List<Client> Clients = new ArrayList<>();
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- models/employes/Employe.java
+++ models/employes/Employe.java
@@ -1,5 +1,13 @@
 package models.employes;
 
 public abstract class Employe {
-    
+    String nom;
+    String prenom;
+    int salaire;
+
+    Employe(String nom, String prenom, int salaire) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.salaire = salaire;
+    }
 }
